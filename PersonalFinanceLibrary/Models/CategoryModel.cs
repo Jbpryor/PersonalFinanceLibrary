5af9ecb023408f5878ddecc8d98e13b3fd4654b6
Jason Pryor <125523612+Jbpryor@users.noreply.github.com>
2024-06-17 09:59:09 -0500

    Updated Category Model for use in newer UI to retrive category Name string

--- PersonalFinanceLibrary/Models/CategoryModel.cs
+++ PersonalFinanceLibrary/Models/CategoryModel.cs
@@ -10,5 +10,29 @@ namespace PersonalFinanceLibrary.Models
     {
         public int Id { get; set; }
         public string? Name { get; set; }
+
+        public static List<CategoryModel> Categories()
+        {
+            return new List<CategoryModel>
+            {
+                new CategoryModel { Id = 1, Name = "Gas" },
+                new CategoryModel { Id = 2, Name = "Groceries" },
+                new CategoryModel { Id = 3, Name = "Other" },
+                new CategoryModel { Id = 4, Name = "Update Card Balance" },
+                new CategoryModel { Id = 7, Name = "Pet" },
+                new CategoryModel { Id = 8, Name = "Car Insurance" }
+            };
+        }
+
+        public static Dictionary<int, string> CategoryDictionary()
+        {
+            return Categories().ToDictionary(category => category.Id, category => category.Name);
+        }
+
+        public static string GetCategoryNameById(int categoryId)
+        {
+            var categoryDictionary = CategoryDictionary();
+            return categoryDictionary.TryGetValue(categoryId, out string categoryName) ? categoryName : null;
+        }
     }
 }
